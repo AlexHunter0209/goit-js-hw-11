6044e82750fdf39d926fd69a6417df84ef932947
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-05-07 22:33:37 +0300

    Create & edit files sass, src

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Notify } from "notiflix";
+import API from './api/api_server.js';
+
+import drawCard from "./templates/drawCard.js";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { search } from "./templates/stateSearch.js";
