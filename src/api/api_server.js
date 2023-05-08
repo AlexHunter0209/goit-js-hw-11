2811c9b82b5c286be90b1cfe0996976fa7e70b8e
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-05-08 22:47:56 +0300

    Edit files

--- src/api/api_server.js
+++ src/api/api_server.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
-import { search } from '../templates/stateSearch';
-
 export default async function getPixabay(url) {
     const res = await axios.get(url);
     return res;
-}
-
-const KEY_PIXABAY = `35768020-57bf980d1d69223dcc2d256cc`;
-
-export default function () {
-  return `https://pixabay.com/api/?key=${KEY_PIXABAY}&image_type=photo&orientation=horizontal&safesearch=true&q=${search.value}&page=${search.page}&per_page=${search.per_page}`;
 }
