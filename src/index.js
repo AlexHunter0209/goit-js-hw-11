b231588336593e05f5859a644b71b0a0678baf16
smulsasha@gmail.com <118166416+AlexHunter0209@users.noreply.github.com>
2023-05-09 17:26:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -58,6 +58,13 @@ function drawGallery(cards) {
 }
 
 function receiveData({ data }) {
+  if (search.page === 1 && data.totalHits === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
   search.visibleBtn = false;
   visible_loadMore();
 
@@ -97,7 +104,7 @@ function receiveData({ data }) {
       gallereyInfo.insertAdjacentHTML('beforeend', marcup);
       galleryImage.refresh();
     }
-    if (search.page === 1 && data.totalHits === 0) {
+    if (search.page === 1) {
       Notify.success(`Hooray! We found ${data.totalHits} images.`);
     }
     searchBtn.disabled = false;
